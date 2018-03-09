538a17411941028176e250a80ee323f7e6af609e
Thomas Dubiel <timidturtle85@gmail.com>
2018-03-09 10:23:17 -0600

    finish edit/update actions and forms

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,13 +11,36 @@ class ArticlesController < ApplicationController
 		@article = Article.new
 	end
 
+	def edit 
+		@article = Article.find(params[:id])
+	end
+
+	def update
+		@article = Article.find(params[:id])
+		@article.update(article_params)
+		if @article.save
+			redirect_to article_path(@article)
+			flash[:success] = "Article successfully updated!"
+		else
+			render :edit
+			flash[:notice] = "Sorry, try again..."
+		end
+	end
+
 	def create 
-		
+		@article = Article.new(article_params)
+		if @article.save
+			redirect_to root_path
+			flash[:succes] = "New article created"
+		else
+			render :new
+			flash[:notice] = "Sorry, invalid values"
+		end
 	end
 
 	private
 
 	def article_params
-		params.require(:articles).permit(:name, :title, :description)
+		params.require(:article).permit(:name, :title, :description)
 	end
 end
