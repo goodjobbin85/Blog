c57511fd74a2a4b03c16a7f17b57c70af1a23989
Thomas Dubiel <timidturtle85@gmail.com>
2018-03-20 22:50:09 -0500

    finish article-user associations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
+	belongs_to :user
 	validates :name, :title, :description, presence: :true
 	validates :description, length: { minimum: 10, maximum: 200 }
+	validates :user_id, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180319225527) do
+ActiveRecord::Schema.define(version: 20180321023920) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20180319225527) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
