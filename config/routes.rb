167d7b3cd5ac649f82e90840e9adc52ed7bc6d9f
Thomas Dubiel <timidturtle85@gmail.com>
2018-03-29 09:53:58 -0500

    login form and sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   root 'pages#index'
   get 'about', to: 'pages#about', as: 'person'
 
@@ -7,4 +13,8 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   post 'users', to: 'users#create'
   resources :users, except: [:new]
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
