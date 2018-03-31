acceb2acb33dbea9ef7f86bb4439ca8fadd3b620
Thomas Dubiel <timidturtle85@gmail.com>
2018-03-31 12:05:59 -0500

    authentication methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,12 @@
 class ArticlesController < ApplicationController
+	before_action :set_article
 
 	def index 
 		@articles = Article.paginate(page: params[:page], per_page: 5)
 	end
+	
 	def show 
-		@article = Article.find(params[:id])
+		
 	end
 
 	def new
@@ -12,11 +14,10 @@ class ArticlesController < ApplicationController
 	end
 
 	def edit 
-		@article = Article.find(params[:id])
+		
 	end
 
 	def update
-		@article = Article.find(params[:id])
 		@article.update(article_params)
 		if @article.save
 			flash[:success] = "Article successfully updated!"
@@ -50,5 +51,9 @@ class ArticlesController < ApplicationController
 
 	def article_params
 		params.require(:article).permit(:name, :title, :description)
+	end
+
+	def set_article
+		@article = Article.find(params[:id])
 	end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController 
+	before_action :set_user, only: [:edit, :update, :show]
 
 	def index 
 		@users = User.paginate(page: params[:page], per_page: 5)
@@ -8,12 +9,10 @@ class UsersController < ApplicationController
 	end
 
 	def show
-		@user = User.find(params[:id])
 		@user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
 	end
 
 	def create 
-		@user = User.new(user_params)
 		if @user.save
 			flash[:success] = "Welcome to Blog #{@user.username}!"
 			redirect_to articles_path
@@ -23,11 +22,10 @@ class UsersController < ApplicationController
 	end
 
 	def edit
-		@user = User.find(params[:id])
+		
 	end
 
 	def update
-		@user = User.find(params[:id])
 		if @user.update(user_params)
 			flash[:success] = "User successfully updated!"
 			redirect_to articles_path
@@ -40,5 +38,9 @@ class UsersController < ApplicationController
 
 	def user_params
 		params.require(:user).permit(:username, :email, :password)
+	end
+
+	def set_user
+		@user = User.find(params[:id])
 	end
 end
