a6a6b2cde0dec69a65adc983ef629da7cf6e4a01
Thomas Dubiel <timidturtle85@gmail.com>
2018-04-15 16:09:14 -0500

    add article_category model and required associations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ApplicationRecord
+	has_many :article_categories
+	has_many :articles, through: :article_categories
 	validates :name, presence: true, uniqueness: true
 	validates :name, length: { maximum: 25, minimum: 4}
 end 
